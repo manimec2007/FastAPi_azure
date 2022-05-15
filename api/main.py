bc409477e75d30c879139b0e262d22d82cb961c4
manimec2007 <42925738+manimec2007@users.noreply.github.com>
2022-05-16 00:40:26 +0530

    Update main.py

--- api/main.py
+++ api/main.py
@@ -2,40 +2,22 @@ from typing import Optional
 
 from fastapi import FastAPI
 from pydantic import BaseModel
+import pandas as pd
 
+df = pd.read_csv('././data/nlpdata.csv')
+
+def get_businessdata(businessname):
+    tmp = df[df.BusinessName == BusinessName]
+    tjson = tmp.to_json()
+    return tjson
 app = FastAPI()
 
-fakedb = []
 
-class Course(BaseModel):
-    id:int
-    name:str
-    price:float
-    is_early_bird: Optional[bool] = None
- 
+@app.get('/')
+def index():
+    return "Diversity Model NLP enpoint service"
 
-@app.get("/")
-def get_home():
-    return {"message": "helloworld"}
-
-
-@app.get("/courses")
-def get_courses():
-    return fakedb
-
-
-@app.get("/courses/{course_id}")
-def get_course(course_id:int):
-    course = course_id - 1
-    return fakedb[course]
-
-
-@app.post("/courses")
-def add_course(course: Course):
-    fakedb.append(course.dict())
-    return fakedb[-1]
-
-@app.delete("/courses")
-def delete_course(course_id: int):
-    fakedb.pop(course_id - 1)
-    return {"message": "deleted successfully"}    
+@app.get('/nlp')
+def get_businessinfo(businessname:str):
+    res = get_businessdata(businessname)
+    return res
